6a6ac5d9ef195d375611af83d855f6563c4aebd8
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2021-02-05 09:46:07 +0100

    Fixed copyright headers
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.ids.manager/src/main/java/org/eclipse/kura/internal/floodingprotection/configuration/FloodingProtectionOptions.java
+++ kura/org.eclipse.kura.ids.manager/src/main/java/org/eclipse/kura/internal/floodingprotection/configuration/FloodingProtectionOptions.java
@@ -9,6 +9,18 @@ import org.eclipse.kura.core.configuration.metatype.Tocd;
 import org.eclipse.kura.core.configuration.metatype.Tscalar;
 import org.eclipse.kura.security.IntrusionDetectionConfiguration;
 
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Eurotech
+ ******************************************************************************/
 public class FloodingProtectionOptions {
 
     private static final String PID = "org.eclipse.kura.floodingprotection.configuration.FloodingProtectionConfigurator";
